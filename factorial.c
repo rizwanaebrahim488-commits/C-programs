1930460c461e6cb24b1c67fc05f9e785a864dc44
Rizwana <rizwanaebrahim488@gmail.com>
2025-12-13 08:36:08 +0530

    Factorial of a number using recursion
    This program calculates factorial of a given number using concept of recursion in c .Recursion is a programming technique where a function calls itself to solve a problem by breaking it into smaller sub problems .

---
+++ factorial.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+ 
+int factorial(int);//fn declaration
+
+void main() {
+      int n,f;
+      
+      printf("Enter a number:  ");
+      scanf("%d",&n);
+      
+      if(n<=0) {
+      printf("Enter a valid number\n");
+      }
+      else{
+      f=factorial(n);//fn calling
+      printf("Factorial of %d is : %d\n",n,f);
+      }
+}
+
+int factorial(int num){
+     int i;
+     
+     if(num<=1) {
+     return 1;//if number is less than 1 return 1
+     }
+     else{//recursive call
+     return num*factorial(num-1);
+     }
+}
